2dc12e77de2fd2edce487abbf7abb73bf3dc5385
regisclaus <regis.claus@gmail.com>
2014-07-18 21:06:20 -0300

    Mario Script
    Adicionei Mario Script

---
+++ unityproject/Assets/Scripts/mario.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mario : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("It's me, Mario!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
